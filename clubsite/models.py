5a46d127097102c90fb72e4474c10276ed37ae1a
Collin Sparks <cklsparks@gmail.com>
2023-06-11 18:37:40 -0400

    added default values and nullable settings

--- clubsite/models.py
+++ clubsite/models.py
@@ -3,16 +3,23 @@ import uuid
 
 
 class User(models.Model):
+    USER = "user"
+    ADMIN = "admin"
+    ROLE_CHOICES = [
+        (USER, "user"),
+        (ADMIN, "admin")
+    ]
+    
     user_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False
     )
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     discord_nick = models.CharField(max_length=30)
-    role = models.CharField(max_length=20)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=USER)
 
     def __str__(self) -> str:
-        return f"User: {self.username}"
+        return f"{self.role.capitalize()}: {self.username}"
 
 class Book(models.Model):
     book_id = models.UUIDField(
@@ -20,11 +27,15 @@ class Book(models.Model):
     )
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    summary = models.TextField(max_length=1000)
+    summary = models.TextField(max_length=1000, blank=True, default="")
     complete = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag")
-    proposer = models.OneToOneField(User, on_delete=models.SET_NULL)
-    readers = models.ManyToManyField(User)
+    proposer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="proposer"
+    )
+    readers = models.ManyToManyField(
+        User, default=list, related_name="readers"
+    )
 
     def __str__(self) -> str:
         return f"Book: {self.title}"
@@ -46,24 +57,30 @@ class Meeting(models.Model):
     date = models.DateTimeField()
     book_section = models.CharField(max_length=50)
     notes = models.TextField(max_length=1000)
-    transcription = models.TextField()
-    transcription_url = models.URLField()
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
-    attendees = models.ManyToManyField(User)
+    transcription = models.TextField(blank=True, default="")
+    transcription_url = models.URLField(blank=True, default="")
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    attendees = models.ManyToManyField(
+        User, blank=True, default=list
+    )
 
     def __str__(self) -> str:
-        return f"Meeting: {self.book.title}, meeting {self.meeting_number}, {self.date}"
+        return f"Meeting: {self.book.title if self.book else '(Book Missing)'}, meeting {self.meeting_number}, {self.date}"
 
 class Post(models.Model):
     post_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False
     )
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, blank=True, default="<Untitled>")
     content = models.CharField(max_length=10_000)
     timestamp = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
-    meeting = models.ForeignKey(Meeting, on_delete=models.SET_NULL)
+    book = models.ForeignKey(
+        Book, on_delete=models.SET_NULL, null=True, default=None
+    )
+    meeting = models.ForeignKey(
+        Meeting, on_delete=models.SET_NULL, null=True, default=None
+    )
 
     def __str__(self) -> str:
         return f"Post by {self.author.username}: {self.title}"
