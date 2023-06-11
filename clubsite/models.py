703da4051ce855df9a2195084bc16205479ee325
Collin Sparks <cklsparks@gmail.com>
2023-06-11 16:07:42 -0400

    progress on models

--- clubsite/models.py
+++ clubsite/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField()
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    discord_nick = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Book(models.Model):
+    id = models.UUIDField()
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    summary = models.TextField(max_length=1000)
+    tags = models.ManyToManyField("Tag")
+    proposer = models.OneToOneField(User, on_delete=models.SET_NULL)
+    readers = models.ManyToManyField(User)
+
+class Tag(models.Model):
+    id = models.UUIDField()
+    name = models.CharField(max_length=50)
