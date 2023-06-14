242328276d70ac7a1d247fb5f6f82ab55f387697
Collin Sparks <cklsparks@gmail.com>
2023-06-14 12:49:55 -0400

    added models to admin panel

--- clubsite/admin.py
+++ clubsite/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import (
+    User, Book, Meeting, Post, Tag
+)
+
 # Register your models here.
+for model in [
+    User,
+    Book,
+    Meeting,
+    Post,
+    Tag
+]:
+    admin.site.register(model)
